abdf0faf91ba8abb1d9b3a44208565da2cf7d325
krishnasri <krishnasrivemuri3@gmail.com>
2023-07-12 01:19:17 +0530

    modified hello world program to palindrome string

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('hello kitty')
+a='radar'
+b='kitty'
+print(type(a),type(b))
+
+# wanted to check - palindrome
+
+print(a==a[::-1])# :: indicates start and stop and step as well
+print(b==b[::-1])
+#a[::-1] - start index 0 stop index 4 step index -1
